b7b59a2595022d6e5f0beb3ce24aa852599d7bc9
Ethan Chen <intervigil@gmail.com>
2011-04-01 19:17:16 -0700

    formatting

--- jni/resample/resample-interface.c
+++ jni/resample/resample-interface.c
@@ -30,77 +30,82 @@
 static int num_channels;
 static struct rs_data **rs;
 
-static void
-downMix(short *out, short *pcm_left, short *pcm_right, int len) {
-    int i;
-    for (i = 0; i < len; i++) {
-        out[i] = (pcm_left[i] / 2 + pcm_right[i] / 2);
-    }
+static void downMix(short *out, short *pcm_left, short *pcm_right, int len)
+{
+	int i;
+	for (i = 0; i < len; i++) {
+		out[i] = (pcm_left[i] / 2 + pcm_right[i] / 2);
+	}
 }
 
 JNIEXPORT void JNICALL Java_net_sourceforge_resample_Resample_downsample
-  (JNIEnv *env, jclass class, jshortArray outputBuffer, jshortArray inputLeft, jshortArray inputRight, jint numSamples)
-{
-    short *input_left, *input_right, *output_buf;
+    (JNIEnv * env, jclass class, jshortArray outputBuffer,
+     jshortArray inputLeft, jshortArray inputRight, jint numSamples) {
+	short *input_left, *input_right, *output_buf;
 
-    output_buf = calloc(numSamples, sizeof(short));
+	output_buf = calloc(numSamples, sizeof(short));
 
-    input_left = (short *) (*env)->GetPrimitiveArrayCritical(env, inputLeft, 0);
-    input_right = (short *) (*env)->GetPrimitiveArrayCritical(env, inputRight, 0);
-    downMix(output_buf, input_left, input_right, numSamples);
-    (*env)->ReleasePrimitiveArrayCritical(env, inputLeft, input_left, 0);
-    (*env)->ReleasePrimitiveArrayCritical(env, inputRight, input_right, 0);
+	input_left =
+	    (short *)(*env)->GetPrimitiveArrayCritical(env, inputLeft, 0);
+	input_right =
+	    (short *)(*env)->GetPrimitiveArrayCritical(env, inputRight, 0);
+	downMix(output_buf, input_left, input_right, numSamples);
+	(*env)->ReleasePrimitiveArrayCritical(env, inputLeft, input_left, 0);
+	(*env)->ReleasePrimitiveArrayCritical(env, inputRight, input_right, 0);
 
-    (*env)->SetShortArrayRegion(env, outputBuffer, 0, numSamples, output_buf);
+	(*env)->SetShortArrayRegion(env, outputBuffer, 0, numSamples,
+				    output_buf);
 }
 
 JNIEXPORT void JNICALL Java_net_sourceforge_resample_Resample_initialize
-  (JNIEnv *env, jclass class, jint inputRate, jint outputRate, jint bufferSize, jint channels)
-{
-    int i;
-    if (!rs) {
-        num_channels = channels;
-        rs = calloc(num_channels, sizeof(struct rs_data *));
-        for (i = 0; i < num_channels; i++) {
-            rs[i] = resample_init(inputRate, outputRate, bufferSize);
-        }
-    } else {
-        /* log it */
+    (JNIEnv * env, jclass class, jint inputRate, jint outputRate,
+     jint bufferSize, jint channels) {
+	int i;
+	if (!rs) {
+		num_channels = channels;
+		rs = calloc(num_channels, sizeof(struct rs_data *));
+		for (i = 0; i < num_channels; i++) {
+			rs[i] =
+			    resample_init(inputRate, outputRate, bufferSize);
+		}
+	} else {
+		/* log it */
 
-    }
+	}
 }
 
 JNIEXPORT jint JNICALL Java_net_sourceforge_resample_Resample_process
-  (JNIEnv *env, jclass class, jshortArray inputBuffer, jshortArray outputBuffer, jint channel, jboolean isLast)
-{
-    int i, in_len, out_len, res;
-    short *in_buf, *out_buf;
+    (JNIEnv * env, jclass class, jshortArray inputBuffer,
+     jshortArray outputBuffer, jint channel, jboolean isLast) {
+	int i, in_len, out_len, res;
+	short *in_buf, *out_buf;
 
-    if (channel >= net_sourceforge_resample_Resample_MAX_CHANNELS) {
-        return -1;
-    }
+	if (channel >= net_sourceforge_resample_Resample_MAX_CHANNELS) {
+		return -1;
+	}
 
-    in_len = (*env)->GetArrayLength(env, inputBuffer);
-    out_len = (*env)->GetArrayLength(env, outputBuffer);
-    in_buf = (short *)(*env)->GetPrimitiveArrayCritical(env, inputBuffer, 0); 
-    out_buf = calloc(out_len, sizeof(short));
+	in_len = (*env)->GetArrayLength(env, inputBuffer);
+	out_len = (*env)->GetArrayLength(env, outputBuffer);
+	in_buf =
+	    (short *)(*env)->GetPrimitiveArrayCritical(env, inputBuffer, 0);
+	out_buf = calloc(out_len, sizeof(short));
 
-    res = resample(rs[channel], in_buf, in_len, out_buf, out_len, isLast);
-    (*env)->SetShortArrayRegion(env, outputBuffer, 0, out_len, out_buf);
-    (*env)->ReleasePrimitiveArrayCritical(env, inputBuffer, in_buf, 0);
+	res = resample(rs[channel], in_buf, in_len, out_buf, out_len, isLast);
+	(*env)->SetShortArrayRegion(env, outputBuffer, 0, out_len, out_buf);
+	(*env)->ReleasePrimitiveArrayCritical(env, inputBuffer, in_buf, 0);
 
-    free(out_buf);
-    return res;
+	free(out_buf);
+	return res;
 }
 
 JNIEXPORT void JNICALL Java_net_sourceforge_resample_Resample_close
-  (JNIEnv *env, jclass class)
-{
-    int i;
-    if (rs) {
-        for (i = 0; i < num_channels; i++) {
-            resample_close(rs[i]);
-        }
-        free(rs);
-    }
+    (JNIEnv * env, jclass class) {
+	int i;
+	if (rs) {
+		for (i = 0; i < num_channels; i++) {
+			resample_close(rs[i]);
+		}
+		free(rs);
+		num_channels = 0;
+	}
 }
