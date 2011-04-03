e76048ebfd7e3389c7550e43d8ec82e71130ebb9
Ethan Chen <intervigil@gmail.com>
2011-04-02 21:15:10 -0700

    formatting

--- src/net/sourceforge/resample/Resample.java
+++ src/net/sourceforge/resample/Resample.java
@@ -9,16 +9,16 @@ public class Resample {
     public static final int CHANNEL_RIGHT = 1;
 
     private static final String RESAMPLE_LIB = "resample";
-    
+
     static {
         System.loadLibrary(RESAMPLE_LIB);
     }
-    
+
     public static native void downsample(short outputBuffer[], short inputLeft[], short inputRight[], int numSamples);
-    
+
     public static native void initialize(int inputRate, int outputRate, int bufferSize, int channels);
-    
+
     public static native int process(short inputBuffer[], short outputBuffer[], int channel, boolean isLast);
-    
+
     public static native void close();
 }
